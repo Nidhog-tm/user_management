501679db2fbf9b1f462e3ce7459ebb1dee007483
Nidhog-tm <nidhog.1031@gmail.com>
2020-09-07 13:15:25 +0900

    ボタン修正

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from 'react-router';
-import { Button, Row, Col } from "reactstrap";
+import { Button, Row } from "reactstrap";
 
 class Content extends Component {
   handleToAboutPage = () => {
