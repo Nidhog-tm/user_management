bd36f23607a88af990ba62ab7039dcbaf08cfcb3
Nidhog-tm <nidhog.1031@gmail.com>
2020-09-12 14:50:06 +0900

    フッターのアイコンを小さく修正

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import logo from "../assets/logo_m.png";
 const Footer = () => (
   <footer className="bg-info p-3 text-center">
     <div>
-      <img className="mb-3 app-logo" src={logo} alt="React logo" width="70" />
+      <img className="mb-3 app-logo" src={logo} alt="React logo" width="50" />
     </div>
     <p>
     since 2020-09 M-Team.INC
