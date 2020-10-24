eef558341a7fc33778e9128bd315b826a4e829dd
Nidhog-tm <nidhog.1031@gmail.com>
2020-10-24 11:42:26 +0900

    リファクタリング

--- src/components/use-dimensions.js
+++ src/components/use-dimensions.js
@@ -10,7 +10,8 @@ export const useDimensions = ref => {
   useEffect(() => {
     dimensions.current.width = ref.current.offsetWidth;
     dimensions.current.height = ref.current.offsetHeight;
-  }, []);
+  });
+  // }, []);
 
   return dimensions.current;
 };
